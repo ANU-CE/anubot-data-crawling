4fd6e05a27d9b58837cb5eb2773eb47804bc8741
Testify <50492313+AstralEUD@users.noreply.github.com>
2023-06-27 19:24:36 +0900

    Vector database (#2)
    * [Update] default:mssql > vector
    
    * [Update] Vector DB Script

--- scripts/anu-dbmap.py
+++ scripts/anu-dbmap.py
@@ -5,73 +5,114 @@ import pandas as pd
 import pyodbc
 import sqlalchemy
 import urllib
+import numpy as np
+import uuid
+
+#for vector database   
+import torch
+
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+from sentence_transformers import SentenceTransformer
+
+from tqdm import tqdm
 
 #for Dev
 from dotenv import load_dotenv
 import os
 
+load_dotenv(verbose=True)
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_DATABASE = os.getenv('DB_DATABASE')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+QDRANT_URL = os.getenv('QDRANT_URL')
+QDRANT_PORT = os.getenv('QDRANT_PORT')
+QDRANT_API_KEY = os.getenv('QDRANT_API_KEY')
+
+model = SentenceTransformer(
+    'jhgan/ko-sroberta-multitask',
+    device="cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu",
+)
+
 def getData(linkarr):
     fullarr = []
     cnt=0
-    for link in linkarr:
-        print(link)
+    for link in tqdm(linkarr):
+        #print(link)
         response = requests.get(link)
         if response.status_code == 200:
             html = response.text
             soup = BeautifulSoup(html, 'html.parser')
         tmparr = []
         id = linkarr[cnt].split('id=')[1]
-        tmparr.append(id)
+        #tmparr.append(id)
 
         try:
             roomname = soup.select_one('#thema_wrapper > div.at-container > div > div > div > div.item_view_box > div.item_info > div.item_title')
             roomname = roomname.text
-            region = roomname.split(' ')[0]
-            roomname = roomname.split(' ')[1]
+            #region = roomname.split(' ')[0]
+            #roomname = roomname.split(' ')[1]
         except:
-            region = ''
+            #region = ''
             roomname = ''
-        tmparr.append(region)
+        #tmparr.append(region)
         tmparr.append(roomname)
 
+        allinfo = ''
         try:
             price = soup.select_one('#thema_wrapper > div.at-container > div > div > div > div.item_view_box > div.item_info > div.item_desc')
             price = price.text
         except:
             price = ''
-        tmparr.append(price)
+        allinfo += price
+        allinfo += '\n'
         
         tmpcnt = 1
 
         while True:
             try:
                 info = soup.select_one(f'#tab1 > div:nth-child(3) > ul > li:nth-child({tmpcnt})').text
-                tmparr.append(info)
+                #tmparr.append(info)
+                allinfo += info
+                allinfo += '\n'
                 tmpcnt+=1
             except:
                 break
+            '''
         if tmpcnt < 9:
             for i in range(tmpcnt, 9):
-                tmparr.append('')
+                tmparr.append('')'''
         try:    
             description = soup.select('#tab1 > div:nth-child(6)')
             description = description.text
         except:
             description = ''
-        tmparr.append(description)
+        allinfo += description
+        allinfo += '\n'
+        tmparr.append(allinfo)
         cnt+=1
         fullarr.append(tmparr)
     return fullarr
 
 def exportData(dbname, fullarr):
-    df = pd.DataFrame(fullarr, columns=['id', 'region', 'name', 'desc', 'info0', 'info1', 'info2', 'info3', 'info4', 'info5', 'info6', 'info7', 'info8'])
+    #df = pd.DataFrame(fullarr, columns=['id', 'region', 'name', 'desc', 'info0', 'info1', 'info2', 'info3', 'info4', 'info5', 'info6', 'info7', 'info8'])
+    df = pd.DataFrame(fullarr, columns=['name', 'info'])
     df.to_csv(f'{dbname}.csv', index=False, encoding='utf-8-sig')
-    # MSSQL DB에 업로드
+    '''# MSSQL DB에 업로드
     params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER="+DB_HOST+","+DB_PORT+";DATABASE="+DB_DATABASE+";UID="+DB_USERNAME+";PWD="+DB_PASSWORD)
     engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
     conn = engine.connect()
     df.to_sql(dbname, con=engine, if_exists='replace', index=False)
-    conn.close()
+    conn.close()'''
+    print(f'{dbname} export complete')
+    return df
 
 def getLinkArr(url):
     #url = 'https://dbmap.andong.ac.kr/bbs/room_list.php'
@@ -91,13 +132,56 @@ def getLinkArr(url):
         linkarr.append(a["href"])
     return linkarr
 
+def vectorize(df, COLLECTION_NAME):
+    client = QdrantClient(
+        url=QDRANT_URL,
+        port=6333, 
+        api_key=QDRANT_API_KEY
+    )
 
-load_dotenv(verbose=True)
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_DATABASE = os.getenv('DB_DATABASE')
+    client.recreate_collection(
+        collection_name=COLLECTION_NAME,
+        vectors_config=models.VectorParams(
+            size=768, 
+            distance=models.Distance.COSINE
+        ),
+    )
+    vectors = []
+    batch_size = 512
+    batch = []
+
+    for doc in tqdm(df["info"].to_list()):
+        batch.append(doc)
+        
+        if len(batch) >= batch_size:
+            vectors.append(model.encode(batch))
+            batch = []
+
+    if len(batch) > 0:
+        vectors.append(model.encode(batch))
+        batch = []
+        
+    vectors = np.concatenate(vectors)
+
+    place_name = df["name"]
+
+    client.upsert(
+        collection_name=COLLECTION_NAME,
+        points=models.Batch(
+            ids=[i for i in range(df.shape[0])],
+            payloads=[
+                {
+                    "text": row["info"],
+                    "name": row["name"] + f", {place_name}",
+                }
+                for _, row in df.iterrows()
+            ],
+            vectors=[v.tolist() for v in vectors],
+        ),
+    )
+
+    #print(client.count(collection_name=COLLECTION_NAME))
+    #print(client.get_collections())
 
 
 '''
@@ -114,15 +198,18 @@ print(info)
 roomlink = 'https://dbmap.andong.ac.kr/bbs/room_list.php'
 linkarr = getLinkArr(roomlink)
 fullarr = getData(linkarr)
-exportData('anubot-dbmap-room', fullarr)
+df = exportData('anubot-dbmap-room', fullarr)
+vectorize(df, 'anubot-dbmap-room')
+
 
 restaruantlink = 'https://dbmap.andong.ac.kr/bbs/restaurant_list.php'
 linkarr = getLinkArr(restaruantlink)
 fullarr = getData(linkarr)
-exportData('anubot-dbmap-restaurant', fullarr)
+df = exportData('anubot-dbmap-restaurant', fullarr)
+vectorize(df, 'anubot-dbmap-restaurant')
 
 tourlink = 'https://dbmap.andong.ac.kr/bbs/tour_list.php'
 linkarr = getLinkArr(tourlink)
 fullarr = getData(linkarr)
-exportData('anubot-dbmap-tour', fullarr)
-
+df = exportData('anubot-dbmap-tour', fullarr)
+vectorize(df, 'anubot-dbmap-tour')
---
+++ vector/vector_search.py
@@ -0,0 +1,87 @@
+import openai
+
+import torch
+
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+from sentence_transformers import SentenceTransformer
+
+from tqdm import tqdm
+
+#for Dev
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_DATABASE = os.getenv('DB_DATABASE')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+QDRANT_URL = os.getenv("QDRANT_URL")
+QDRANT_PORT = os.getenv("QDRANT_PORT")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+
+print(QDRANT_API_KEY, QDRANT_URL)
+
+COLLECTION_NAME = 'anubot-dbmap-room'
+
+qdrant_client = QdrantClient(
+    url = QDRANT_URL,
+    port=6333, 
+    api_key = QDRANT_API_KEY
+)
+
+retrieval_model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+
+openai.api_key = OPENAI_API_KEY
+
+def build_prompt(question: str, references: list) -> tuple[str, str]:
+    prompt = f"""
+    다음과 같은 질문을 한 친구에게 조언을 하고 있습니다.: '{question}'
+
+    글에서 가장 관련성이 높은 구절을 선택하여 답변의 출처로 사용할겁니다. 당신의 대답에 그것들을 인용하세요.
+
+    References:
+    """.strip()
+
+    references_text = ""
+
+    for i, reference in enumerate(references, start=1):
+        text = reference.payload["text"].strip()
+        references_text += f"\n[{i}]: {text}"
+
+    prompt += (
+        references_text
+        + "\n참고문헌 인용 방법: 이것은 인용문[1]입니다. 이것도 [3]. 그리고 이것은 인용문이 많은 문장입니다 [2][3].\n답:"
+    )
+    return prompt, references_text
+
+def ask(question: str):
+    similar_docs = qdrant_client.search(
+        collection_name='anubot-dbmap-tour',
+        query_vector=retrieval_model.encode(question),
+        limit=3,
+        append_payload=True,
+    )
+
+    prompt, references = build_prompt(question, similar_docs)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=250,
+        temperature=0.2,
+    )
+
+    return {
+        "response": response["choices"][0]["message"]["content"],
+        "references": references,
+    }
+
+print(ask("괜찮은 관광지 하나만 추천해줘."))
+print(qdrant_client.get_collections())
